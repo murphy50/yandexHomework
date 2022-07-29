af8bb0d9e76b6d0881bfd699b282cb7950ba4822
Murphy <ardain31@gmail.com>
2022-07-29 04:03:26 +0400

    init

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by murphy on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
